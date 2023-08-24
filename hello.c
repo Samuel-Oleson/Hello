93389a26c0c8dc68aadabb150636f71a83ed5314
Samuel-Oleson <142913999+Samuel-Oleson@users.noreply.github.com>
2023-08-24 13:30:28 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Samuel Oleson
+ * Date: 2023/08/24
+ *
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int, argc, char **arggv) {
+    Print("Samuel Oleson/n");
+
+    Return 0;
+}
